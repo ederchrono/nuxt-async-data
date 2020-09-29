17337da34062e09fcb1c4934e55fe121bccd4a2d
Eder Díaz <ederchrono@gmail.com>
2020-09-28 23:59:58 -0500

    :sparkles: initial commit

---
+++ pages/index.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <h1>Hello world!</h1>
+    <pre>{{ apiResponse }}</pre>
+  </div>
+</template>
+
+<script>
+import axios from 'axios'
+axios.defaults.baseURL = 'http://localhost:8000/api/v1'
+
+export default {
+  async asyncData() {
+    const dataFromLocalHost = await axios.get('message')
+    return {
+      apiResponse: dataFromLocalHost.data
+    }
+  }
+}
+</script>
